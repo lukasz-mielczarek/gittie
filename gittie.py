bfa40733c7ec2e3c7bc4d0bcae9f07e3d6933b19
lukasz-mielczarek <lukasz.mielczarek1989@gmail.com>
2019-12-10 11:22:29 +0100

    added Hahaha2

--- gittie.py
+++ gittie.py
@@ -1,7 +1,7 @@
 def introduction():
     print("hello, im gittie!")
 def joke():
-    print("why did the chicken cross the road? \n cause it needed to get to the other side.")
+    print("why did the chicken cross the road? \n cause it needed to get to the other side. \n HAHAHAHA")
 
 def add():
     name = input("Whats your name?": )
